d4000b08bd7e4f096346a2d9840183144327f9bd
developer-nieem <naimhossenwork@gmail.com>
2023-07-25 14:37:54 +0600

    solved first problem

---
+++ problem-solving.js
@@ -0,0 +1,28 @@
+// Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+/* 
+const reverseString =  (inputString) => {
+
+    let reversString =  '';
+
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        const element = i;
+      
+        reversString = reversString + inputString.charAt(i)
+        
+    }
+
+    return reversString;
+
+}
+
+const inputStr =  "hello world" ;
+
+const result = reverseString(inputStr);
+
+console.log(result);
+ */
+
+
+// Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
+
